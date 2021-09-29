e28fcecbe6f34534534397aab05891cb6a8029f4
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-09-28 23:43:48 -0500

    created init function and a function to write ReadMe file

--- Develop/index.js
+++ Develop/index.js
@@ -3,77 +3,85 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [
-  {
-    type: "input",
-    projectName: "projectName",
-    message: "What is your project name?",
-  },
-  {
-    type: "input",
-    description: "description",
-    message: "Provide a brief description of your project:",
-  },
-  {
-    type: "input",
-    tableOfContents: "tableOfContents",
-    message: "Name your table of contents.",
-  },
-  {
-    type: "input",
-    installation: "installation",
-    message: "",
-  },
-  {
-    type: "input",
-    usage: "usage",
-    message: "Usage",
-  },
-  {
-    type: "input",
-    license: "license",
-    message: "Program License",
-  },
-  {
-    type: "input",
-    contributing: "contributing",
-    message: "Contributing",
-  },
-  {
-    type: "input",
-    tests: "tests",
-    message: "Tests",
-  },
-  {
-    type: "input",
-    questions: "questions",
-    message: "Questions",
-  },
-  {
-    type: "input",
-    repository: "repository",
-    message: "Repository URL:",
-  },
-  {
-    type: "input",
-    github: "github username",
-    message: "What is your GitHub Username?",
-  },
-  {
-    type: "input",
-    email: "email",
-    message: "What is your email address?",
-  },
-];
+const prompQuestions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      projectName: "projectName",
+      message: "What is your project name?",
+    },
+    {
+      type: "input",
+      description: "description",
+      message: "Provide a brief description of your project:",
+    },
+    {
+      type: "input",
+      tableOfContents: "tableOfContents",
+      message: "Name your table of contents.",
+    },
+    {
+      type: "input",
+      installation: "installation",
+      message: "",
+    },
+    {
+      type: "input",
+      usage: "usage",
+      message: "Usage",
+    },
+    {
+      type: "input",
+      license: "license",
+      message: "Program License",
+    },
+    {
+      type: "input",
+      contributing: "contributing",
+      message: "Contributing",
+    },
+    {
+      type: "input",
+      tests: "tests",
+      message: "Tests",
+    },
+    {
+      type: "input",
+      questions: "questions",
+      message: "Questions",
+    },
+    {
+      type: "input",
+      repository: "repository",
+      message: "Repository URL:",
+    },
+    {
+      type: "input",
+      github: "github username",
+      message: "What is your GitHub Username?",
+    },
+    {
+      type: "input",
+      email: "email",
+      message: "What is your email address?",
+    },
+  ]);
+};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
-init();
+const init = () => {
+    prompQuestions()
+    // TODO: Create a function to write README file
+    .then((answers) => fs.writeFileSync('index.html', generateHTML(anwers)))
+    .then(() => console.log('Successfully written to index.html'))
+    .catch((err) => console.error(err));
+function writeToFile(fileName, data) {}
+};
 
 // run npm init
 // run npm i inquirer
