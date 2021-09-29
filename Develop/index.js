f39c2ebb8bc18eb9a2929a729e926d4ec7d8ce7d
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-09-28 21:58:59 -0500

    updated pseudocode for development. Created const for required packages

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -14,3 +16,26 @@ init();
 
 // run npm init
 // run npm i inquirer
+// create a boiler plate html
+// const fs = require('fs')
+// const inquirer = require('inquirer')
+// Prompt user for information about application repository
+// generate readme with title of project and sections
+// entitled Description, Table of Contents,
+// Installation, Usage, License, Contributing, Tests,
+// and Questions
+// In the Questions section of the ReadMe, we need a link
+// to my GitHub profile
+// Create prompts
+// What is the link to your repository?
+// What is the title of your project?
+// Description of project.
+// Do you want a table of contents?
+// Installation instructions.
+// Usage info.
+// License.
+// Contributing.
+// Tests.
+// Questions.
+// Enter your GitHub Username:
+// What is your email address?
